d7fb904a5707a83d3c950392981a868408acde3a
oyku <49123232+Goyku9@users.noreply.github.com>
2021-05-29 21:20:04 +0300

    swagger added

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/PositionControllers.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/PositionControllers.java
@@ -1,5 +1,32 @@
 package kodlamaio.hrms.api.controllers;
 
-public class PositionControllers {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.PositionService;
+import kodlamaio.hrms.entities.concretes.Position;
+
+@RestController
+@RequestMapping("/api/positions")
+@Service
+public class PositionControllers {
+	
+	private PositionService positionService;
+    
+	@Autowired
+	public PositionControllers(PositionService positionService) {
+		super();
+		this.positionService = positionService;
+	}
+	
+@GetMapping("/getall")	
+public List<Position> getAll(){
+	return this.positionService.getAll();
+	
+}
 }
--- hrms/src/main/java/kodlamaio/hrms/business/concretes/PositionManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/PositionManager.java
@@ -2,10 +2,13 @@ package kodlamaio.hrms.business.concretes;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import kodlamaio.hrms.business.abstracts.PositionService;
 import kodlamaio.hrms.dataAccess.abstracts.PositionDao;
 import kodlamaio.hrms.entities.concretes.Position;
 
+@Service
 public class PositionManager implements PositionService{
 	
 	PositionDao positionDao;
